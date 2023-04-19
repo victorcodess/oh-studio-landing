9a0212e7dabea93c98ed0699ddd2c453e66060d9
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-04-19 15:54:38 +0100

    fix: reduce leading in contact text

--- src/contact-section/Contact.jsx
+++ src/contact-section/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <section className="flex flex-col pt-[114px] md:pt-[67px] md:pb-[67px] lg:py-[144px] pb-[114px] items-center justify-center text-[30px] md:text-[38px] font-bold ">
       <h2>Let's work together.</h2>
-      <h2 className="text-[#7f7f7f] hover:text-[#7f7f7f]/50 cursor-pointer">
+      <h2 className="text-[#7f7f7f] hover:text-[#7f7f7f]/50 cursor-pointer -mt-2">
         Get in touch.
       </h2>
     </section>
