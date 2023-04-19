30c4c66bb7edafc69198fd572f3dadd71eeebd8c
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-04-19 09:18:10 +0100

    feat: add fonts, complete navbar

--- src/NavBar/Navbar.jsx
+++ src/NavBar/Navbar.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex fixed translate-x-[-50%] left-[50%] mx-auto top-5 py-1 px-1  bg-[#e8e5e480]  rounded-full text-[14px]">
-      <h4 className="py-[12px] px-[25px] rounded-full bg-white">Home</h4>
-      <h4 className="py-[12px] px-[25px] rounded-full">Profile</h4>
-      <h4 className="py-[12px] px-[25px] rounded-full">Contact</h4>
+    <nav className="flex fixed translate-x-[-50%] left-[50%] mx-auto top-5 py-1 px-1  bg-[#e8e5e480]/50 backdrop-blur-lg  rounded-full text-[14px] max-w-[280px] gap-[4px]">
+      <button className="w-[88px] h-[40px] text-center rounded-full bg-white">
+        Home
+      </button>
+      <button className="w-[88px] h-[40px] text-center rounded-full">
+        Profile
+      </button>
+      <button className="w-[88px] h-[40px] text-center rounded-full">
+        Contact
+      </button>
     </nav>
   );
 };
